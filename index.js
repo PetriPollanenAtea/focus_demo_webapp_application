2ed63414655fd507753dd758cf7586a68bfb55bf
Petri Pöllänen <petri.pollanen@atea.fi>
2024-10-07 12:48:34 +0300

    Updates

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
 const express = require("express");
-const path = require('path');
+const path = require("path");
+
+/**
+ * App Variables
+ */
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || "3000";
 
+/**
+ *  App Configuration
+ */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+/**
+ * Routes Definitions
+ */
 app.get("/", (req, res) => {
-  res.status(200).send("Tervetuloa Focukseen!");
-});
+    res.render("index", { title: "Home" });
+  });
 
-
+/**
+ * Server Activation
+ */
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Listening to requests on http://localhost:${port}`);
 });
