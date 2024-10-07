26a2ecf237688215d8354c1ffa33e242c8c8c261
Petri Pöllänen <petri.pollanen@atea.fi>
2024-10-07 09:37:22 +0300

    Added background image

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    s = `<H3>Tervetuloa Focukseen ${name}!</H3>`
+    s = `<p style="background-image: url('taustakuva01.jpg');"><H3>Tervetuloa Focukseen ${name}!</H3>`
     return res.send(s);
 })
 
