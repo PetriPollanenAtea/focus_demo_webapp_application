52356b1df98c535e24905396f3fe2e21e1f887b8
Petri Pöllänen <petri.pollanen@atea.fi>
2024-10-07 12:25:12 +0300

    Updates

--- index.js
+++ index.js
@@ -1,48 +1,13 @@
-var path = require('path');
-var http = require('http');
-var fs = require('fs');
-
-var dir = path.join(__dirname, 'public');
-
+const express = require("express");
+const path = require('path');
+const app = express();
 const port = process.env.PORT || 3000;
 
-var mime = {
-    html: 'text/html',
-    txt: 'text/plain',
-    css: 'text/css',
-    gif: 'image/gif',
-    jpg: 'image/jpeg',
-    png: 'image/png',
-    svg: 'image/svg+xml',
-    js: 'application/javascript'
-};
-
-var server = http.createServer(function (req, res) {
-  var reqpath = req.url.toString().split('?')[0];
-  if (req.method !== 'GET') {
-      res.statusCode = 501;
-      res.setHeader('Content-Type', 'text/plain');
-      return res.end('Method not implemented');
-  }
-  var file = path.join(dir, reqpath.replace(/\/$/, '/index.html'));
-  if (file.indexOf(dir + path.sep) !== 0) {
-      res.statusCode = 403;
-      res.setHeader('Content-Type', 'text/plain');
-      return res.end('Forbidden');
-  }
-  var type = mime[path.extname(file).slice(1)] || 'text/plain';
-  var s = fs.createReadStream(file);
-  s.on('open', function () {
-      res.setHeader('Content-Type', type);
-      s.pipe(res);
-  });
-  s.on('error', function () {
-      res.setHeader('Content-Type', 'text/plain');
-      res.statusCode = 404;
-      res.end('Not found');
-  });
+app.get("/", (req, res) => {
+  res.status(200).send("Tervetuloa Focukseen!");
 });
 
-server.listen(port, function () {
+
+app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
